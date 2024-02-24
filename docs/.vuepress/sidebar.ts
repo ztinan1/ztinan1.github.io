2d3d82b8fbce7152b0448c631408a8984e307bd9
ztinan1 <160204293+ztinan1@users.noreply.github.com>
2024-02-24 15:03:29 +0800

    Update sidebar.ts

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -3,8 +3,8 @@ import { sidebar } from "vuepress-theme-hope";
 // 精选图标：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default sidebar({
   "": [
-    "/DailyRoutine",
-    "/Fitness",
+   // "/DailyRoutine",
+   // "/Fitness",
     // 读书笔记架构更换到 docsify，不能使用相对链接
     { text: "读书笔记", icon: "read", link: "https://1patent.cn/reading/" },
     // 指定显示页面
